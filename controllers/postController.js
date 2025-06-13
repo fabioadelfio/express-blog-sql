d1c9c9523ef89a483f8c3367a5072919a0fc299f
fabioadelfio <adelfiofabio07@gmail.com>
2025-06-13 14:40:36 +0200

    Milestone 4

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,6 +22,17 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
+
+    const postId = req.params.id;
+    const sql = `SELECT * FROM posts WHERE id = ?`;
+
+    connection.query(sql, [postId], (err, results) => {
+        if (err) return res.status(500).json({ error: `Database query Failed` });
+        if(results.length === 0) return res.status(404).json({ error: `Pizza not found` });
+        res.json(results[0]);
+    });
+
+
     // const id = parseInt(req.params.id);
     // const post = posts.find(p => p.id === id);
     // if(!post) {

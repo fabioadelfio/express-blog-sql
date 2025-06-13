1e9366192c9e2b8c4d429d52bfc2d4eb9bf96377
fabioadelfio <adelfiofabio07@gmail.com>
2025-06-13 13:59:09 +0200

    Milestone 1

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require(`express`);
+const app = express();
+const db = require(`./data/db`);
+const postsRouter = require('./routers/posts');
+
+
+app.use(express.static(`public`));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`Server del mio blog`);
+});
+
+app.get(`/bacheca`, (req, res) => {
+    res.json({ posts });
+});
+
+// Routers
+app.use(`/posts`, postsRouter);
+
+// Middlewares
+const notFound = require(`./middlewares/notFound`);
+app.use(notFound);
+
+const errorHandler = require(`./middlewares/errorHandler`);
+app.use(errorHandler);
+
+app.listen(3306, () => {
+    console.log(`Server listening...`);
+});
